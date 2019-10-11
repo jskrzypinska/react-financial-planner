83b5f8935ae9482c3d95d5d89129ba355071e948
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-10-11 16:11:53 +0100

    Updates

---
+++ precache-manifest.0a7ca9c0a9badea5ecdc4aac052c44dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f852c878a175c2f6e5bf2c90efc4a852",
+    "url": "/react-financial-planner/index.html"
+  },
+  {
+    "revision": "1c90c5f723cda6f7c96e",
+    "url": "/react-financial-planner/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "0370a83a9c0baec0ad89",
+    "url": "/react-financial-planner/static/css/main.5de54711.chunk.css"
+  },
+  {
+    "revision": "1c90c5f723cda6f7c96e",
+    "url": "/react-financial-planner/static/js/2.5401a779.chunk.js"
+  },
+  {
+    "revision": "0370a83a9c0baec0ad89",
+    "url": "/react-financial-planner/static/js/main.f727bdb0.chunk.js"
+  },
+  {
+    "revision": "69f38f7ab35cf1990261",
+    "url": "/react-financial-planner/static/js/runtime~main.a774227a.js"
+  },
+  {
+    "revision": "f862badb34ea3362f3b7d0756383830b",
+    "url": "/react-financial-planner/static/media/costs.f862badb.svg"
+  },
+  {
+    "revision": "0027f4d633dfed7c1c64c839336943e5",
+    "url": "/react-financial-planner/static/media/logo.0027f4d6.svg"
+  },
+  {
+    "revision": "532bb59563a277ba72b1e2eeeada0abe",
+    "url": "/react-financial-planner/static/media/pig.532bb595.svg"
+  },
+  {
+    "revision": "8da0e5afab3f1ee73176f894d378ee76",
+    "url": "/react-financial-planner/static/media/revenues.8da0e5af.svg"
+  }
+]);
